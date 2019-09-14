6a29ae7dd8e89788c19f150935d769ce52818943
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-14 17:55:27 -0300

    Criado o repositorio

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9bc1a23463c151ce3cc6ddc4d124f263',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware); // Define o middleware somente para as rotas abaixo
+
+routes.put('/users', UserController.update);
+
+export default routes;
