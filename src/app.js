8d82b3ad690775b90490c195716b70e36a52947b
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-18 23:16:17 -0300

    Tratamento de exceçoes com Sentry pt 1

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+import 'express-async-errors';
 import routes from './routes';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
-  }
+}
 
   middlewares() {
+    // The request handler must be the first middleware on the app
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.server.use(express.json());
     // Inserir arquivos estáticos como css, html etc
     this.server.use(
@@ -21,6 +30,9 @@ class App {
 
   routes() {
     this.server.use(routes);
+
+    // The error handler must be before any other error middleware and after all controllers
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://1d92c26215ec468c8a81cf1fa5145006@sentry.io/1731311',
+};
